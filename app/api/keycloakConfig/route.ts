e04a24513cf9557b874abbdfa4e33df2cf834082
JANINE <478518@student.fontys.nl>
2025-01-08 09:03:58 +0100

    test 2

---
+++ app/api/keycloakConfig/route.ts
@@ -0,0 +1,12 @@
+﻿import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({
+        realm: process.env.KEYCLOAK_REALM,
+        authServerUrl: process.env.KEYCLOAK_URL,
+        clientId: process.env.KEYCLOAK_CLIENT_ID,
+        sslRequired: 'external',
+        publicClient: true,
+        confidentialPort: 0,
+    });
+}
