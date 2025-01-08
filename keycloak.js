7af5d3dcd3be5cbff08bcac517543bb8a300a6ad
JANINE <478518@student.fontys.nl>
2025-01-08 09:47:17 +0100

    keycloak thing, idfk anymore

--- keycloak.js
+++ keycloak.js
@@ -20,7 +20,11 @@ const keycloak = typeof window !== "undefined"
         realm: process.env.KEYCLOAK_REALM,
         clientId: process.env.KEYCLOAK_CLIENT_ID,
     })
-    : null;
+    : new Keycloak({
+        url: process.env.KEYCLOAK_URL,
+        realm: process.env.KEYCLOAK_REALM,
+        clientId: process.env.KEYCLOAK_CLIENT_ID
+    })
 
 export const KeycloakProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
